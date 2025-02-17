8f5c98f353bc856299dbe3cc5278093b0e33bbc2
meadosc <meadosc@gmail.com>
2025-02-17 11:02:29 -0800

    improve schema addition logic

--- comp_dict/dict_schema.py
+++ comp_dict/dict_schema.py
@@ -5,11 +5,5 @@ class DictSchema:
 		self.count = 1
 
 	def add(self, fname, fname_index):
-		"""
-		try:
-			self.fnames[fname].append(fname_index)
-		except KeyError:
-			self.fnames[fname] = [fname_index]
-		"""
-		self.fnames.get(fname, [fname_index]).append(fname_index)
+		self.fnames.setdefault(fname, []).append(fname_index)
 		self.count += 1
